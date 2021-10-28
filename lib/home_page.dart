0ca088af652525d7251489f6c9535991e24aed60
swayam <smswayam06@gmail.com>
2021-10-28 23:24:19 +0100

    updated BarDatum

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,7 +6,6 @@ import 'package:racing_data_bar/racing_data_bars.dart';
 /// Created By AURO (aurosmruti@smarttersstudio.com) on 8/20/2020 12:50 AM
 ///
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -19,26 +18,16 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(),
       body: Column(
         children: [
-          RacingDataBars(
-              data: [
-                BarDatum(
-                  title: 'A',
-                  value: 50
-                ),
-                BarDatum(
-                  title: 'C',
-                  value: 10
-                ),
-                BarDatum(
-                  title: 'D',
-                  value: 80
-                ),
-                BarDatum(
-                    title: 'B',
-                    value: 100
-                ),
-              ]
-          )
+          RacingDataBars(data: [
+            BarDatum(title: 'A', value: 50),
+            BarDatum(title: 'C', value: 10),
+            BarDatum(title: 'D', value: 80),
+            BarDatum(title: 'B', value: 100),
+            BarDatum(title: 'P', value: 40),
+            BarDatum(title: 'Q', value: 90),
+            BarDatum(title: 'R', value: 50),
+            BarDatum(title: 'T', value: 150),
+          ])
         ],
       ),
     );
